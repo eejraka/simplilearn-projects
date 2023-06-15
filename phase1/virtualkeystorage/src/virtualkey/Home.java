b0012a35a5083c908f90e697daad5c373c4aa151
eejraka <57679541+eejraka@users.noreply.github.com>
2023-06-15 22:04:38 +0530

    file handling and basic exceptions

--- phase1/virtualkeystorage/src/virtualkey/Home.java
+++ phase1/virtualkeystorage/src/virtualkey/Home.java
@@ -1,5 +1,8 @@
 package virtualkey;
 import java.util.*;
+import java.io.File;
+import java.io.IOException;
+
 
 interface References{
 	Set<String> repo = new TreeSet<>();
@@ -7,6 +10,15 @@ interface References{
 }
 
 public class Home implements References {
+	public static void delAll() {
+		File dir = new File("C:\\Users\\cwc\\repos\\simplilearn-projects\\phase1\\virtualkeystorage\\src\\files");
+		for(File file : dir.listFiles()) {
+			if(!file.isDirectory()) {
+				file.delete();
+			}
+		}
+	}
+	
 	public static void printEle() {
 		if(!repo.isEmpty()) {
 			for(String temp : repo) {
@@ -29,8 +41,23 @@ public class Home implements References {
 		System.out.println("Insert Key to add to repository: ");
 		sc.nextLine();
 		String temp = sc.nextLine();
-		repo.add(temp);
-		System.out.println("SUCCESS: Key '"+temp+"' added to repository");
+
+		try {
+			File anew = new File("C:\\Users\\cwc\\repos\\simplilearn-projects\\phase1\\virtualkeystorage\\src\\files\\"+temp+".txt");
+			if(anew.createNewFile()) {
+				System.out.println("SUCCESS: Key '"+temp+"' added to repository");
+				repo.add(temp);
+			}
+			else {
+				System.out.println("ERROR: Key already exists");
+			}
+		}
+		catch(IOException e) {
+			delAll();
+			System.out.println("An error occured");
+			System.exit(0);
+		}
+		
 		return;
 	}
 	
@@ -39,6 +66,8 @@ public class Home implements References {
 		sc.nextLine();
 		String t1 = sc.nextLine();
 		if(repo.contains(t1)) {
+			File rem = new File("C:\\Users\\cwc\\repos\\simplilearn-projects\\phase1\\virtualkeystorage\\src\\files\\"+t1+".txt");
+			rem.delete();
 			repo.remove(t1);
 			System.out.println("SUCCESS: Key "+t1+" removed from repository");
 		}
@@ -69,40 +98,36 @@ public class Home implements References {
 			System.out.println("3. Search for Key");
 			System.out.println("4. Return to Main Menu");
 			System.out.println("Select desired option by typing an integer: ");
-			int next_choice = sc.nextInt();
-			
-			switch(next_choice) {
-			case 1:
-				//add file
-				addKey();
-				break;
-			case 2:
-				//delete file
-				delKey();
-				break;
-			case 3:
-				//search file
-				srcKey();
-				break;
-			case 4:
-				return;
-			default:
-				System.out.println("Please make a valid selection.");
+			try{
+				int next_choice = sc.nextInt();
+				switch(next_choice) {
+				case 1:
+					//add file
+					addKey();
+					break;
+				case 2:
+					//delete file
+					delKey();
+					break;
+				case 3:
+					//search file
+					srcKey();
+					break;
+				case 4:
+					return;
+				default:
+					System.out.println("Please make a valid selection.");
+				}
+			} catch (InputMismatchException e) {
+				System.out.println("Invalid input. Goodbye!");
+				delAll();
+				System.exit(0);
 			}
+			
 		}
 	}
 	
-	public static void main(String[] args) {
-//		display welcome screen(app name and dev details)
-//		next show options for usage
-//		1)return current file names in ascending order
-//		2)should contain the following:
-//			add file to directory(ignore case sensitivity)
-//			delete file(should be case sensitive) & FNF is not existing
-//			search specific file(should be case sensitive)
-//		option to go back to home page
-//		3)option to terminate the app
-		
+	public static void main(String[] args) {		
 		homeAbout();
 		
 		while(true) {
@@ -112,22 +137,30 @@ public class Home implements References {
 			System.out.println("2. Additional Operations");
 			System.out.println("3. Exit Application");
 			System.out.println("Select desired option by typing an integer: ");
-			int choice = sc.nextInt();
-			
-			switch(choice) {
-			case 1:
-				printEle();
-				break;
-				
-			case 2:
-				subMenu();
-				break;
-			case 3:
-				System.out.println("Goodbye!");
+
+			try{
+				int choice = sc.nextInt();
+				switch(choice) {
+				case 1:
+					printEle();
+						break;
+						
+				case 2:
+					subMenu();
+					break;
+				case 3:
+					System.out.println("Goodbye!");
+					delAll();
+					System.exit(0);
+				default:
+					System.out.println("Please make a valid selection.");
+				}
+			} catch (InputMismatchException e) {
+				System.out.println("Invalid input. Goodbye!");
+				delAll();
 				System.exit(0);
-			default:
-				System.out.println("Please make a valid selection.");
-			}			
+			}
+						
 		}
 	
 	}
